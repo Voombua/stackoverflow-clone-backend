787165f96dc04f09d3f60ba9a7046622244eb6dc
ianshiundu <ian.shiundu@gmail.com>
2018-09-23 17:31:29 +0300

    refactor to use slick

---
+++ src/main/scala/com/voombua/ServiceMain.scala
@@ -0,0 +1,41 @@
+package com.voombua
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.stream.ActorMaterializer
+import com.voombua.routes.UserRoutes
+import com.voombua.utils.{Config, MigrationConfig}
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+object ServiceMain extends App with Config with MigrationConfig {
+
+  implicit val system: ActorSystem = ActorSystem() // ActorMaterializer requires an implicit ActorSystem
+  implicit val ec: ExecutionContextExecutor = system.dispatcher // bindingFuture.map requires an implicit ExecutionContext
+
+  val api = new UserRoutes().routes // the RestApi provides a Route
+
+  implicit val materializer: ActorMaterializer = ActorMaterializer() // bindAndHandle requires an implicit materializer
+
+  migrate()
+
+  val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(api, httpInterface, httpPort) // starts the HTTP server
+
+  val log = Logging(system.eventStream, "stack-overflow-clone")
+
+  try {
+    //    Here we start the HTTP server and log the info
+    bindingFuture.map { serverBinding ⇒
+      log.info(s"RestApi bound to ${serverBinding.localAddress}")
+    }
+  } catch {
+    //    If the HTTP server fails to start, we throw an Exception and log the error and close the system
+    case ex: Exception ⇒
+      log.error(ex, "Failed to bind to {}:{}!", httpInterface, httpPort)
+      //      System shutdown
+      system.terminate()
+  }
+}r
+
