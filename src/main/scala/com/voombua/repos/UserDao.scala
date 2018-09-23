1529ea29669cb4881f543bc5176ebc331a0d05ea
ianshiundu <ian.shiundu@gmail.com>
2018-09-23 17:35:55 +0300

    add Dao for Postgres DB

---
+++ src/main/scala/com/voombua/repos/UserDao.scala
@@ -0,0 +1,19 @@
+package com.voombua.repos
+
+import com.voombua.models.UserId
+import com.voombua.models.User
+import slick.jdbc.PostgresProfile.api._
+
+import scala.concurrent.Future
+
+object UserDao extends BaseDao {
+  def findAll: Future[Seq[User]] = usersTable.result
+  def findById(userId: UserId): Future[User] = usersTable.filter(_.id === userId).result.head
+  def create(user: User): Future[UserId] = usersTable returning usersTable.map(_.id) += user
+  def update(newUser: User, userId: UserId): Future[Int] = usersTable.filter(_.id === userId)
+    .map(user => (user.username, user.password, user.email))
+    .update((newUser.userName, newUser.password, newUser.email))
+
+  def delete(userId: UserId): Future[Int] = usersTable.filter(_.id === userId).delete
+
+}
