1fe3cff4fd975172f6f2023c436486e75a90c012
ianshiundu <ian.shiundu@gmail.com>
2018-09-23 19:20:22 +0300

    return database url

--- src/main/scala/com/voombua/utils/MigrationConfig.scala
+++ src/main/scala/com/voombua/utils/MigrationConfig.scala
@@ -5,7 +5,7 @@ import org.flywaydb.core.Flyway
 trait MigrationConfig extends Config {
 
   private val flyway = new Flyway()
-  flyway.setDataSource("jdbc:" + databaseUrl, databaseUser, databasePassword)
+  flyway.setDataSource(databaseUrl, databaseUser, databasePassword)
 
   def migrate() = {
     flyway.migrate()
