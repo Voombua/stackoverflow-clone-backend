285a1cdf18b2cefcc9610c72bfe2a87b06b30e56
ianshiundu <ian.shiundu@gmail.com>
2018-09-25 19:54:35 +0300

    migration V3

--- src/main/resources/db/migration/V3__Create_new_timestamp_field.sql
+++ src/main/resources/db/migration/V3__Create_new_timestamp_field.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   username      VARCHAR   NOT NULL,
   email      VARCHAR   NOT NULL,
   password    VARCHAR   NOT NULL DEFAULT md5(random() :: TEXT),
-  created_at TIMESTAMP NOT NULL DEFAULT,
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP
 );
