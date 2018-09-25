fc77302b60ef7827373d9e0d3f53a90bfe837a68
ianshiundu <ian.shiundu@gmail.com>
2018-09-25 19:25:22 +0300

    add Get all users endpoint

--- src/main/scala/com/voombua/routes/UserRoutes.scala
+++ src/main/scala/com/voombua/routes/UserRoutes.scala
@@ -1,28 +1,30 @@
 package com.voombua.routes
 
+import java.sql.Timestamp
+import java.time.Instant
+
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
-import com.voombua.commands.UserCommands
-import com.voombua.mapping.JsonMapping
-import com.voombua.messages.LoginRequestMessage
-import com.voombua.models.User
-import com.voombua.repos.UserDao
+import com.voombua.mapping.JsonProtocol
+import com.voombua.models.{ User, UserComponent }
+import java.util.UUID
 
 import scala.concurrent.duration._
-import scala.util.{ Failure, Success }
+import scala.language.postfixOps
 
-class UserRoutes extends JsonMapping {
+class UserRoutes(repo: UserComponent#UserRepository) extends JsonProtocol {
   val service = "users"
   val version = "v1"
-  protected val createMember: Route = {
-    pathPrefix(service / version / "member") {
+  protected val createUser: Route = {
+    pathPrefix(service / version / "user") {
       post {
         pathEndOrSingleSlash {
           withRequestTimeout(5 minutes) {
 
             entity(as[User]) { request ⇒
-              complete((StatusCodes.Created, UserDao.create(request)))
+              val r = request.copy(id = Some(UUID.randomUUID()), created = Some(Timestamp.from(Instant.now())))
+              complete((StatusCodes.Created, repo.insert(r)))
             }
           }
         }
@@ -30,29 +32,18 @@ class UserRoutes extends JsonMapping {
     }
   }
 
-  protected val loginByEmail: Route = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-    pathPrefix(service / version / "login") {
-      post {
+  protected val getUsers: Route = {
+    pathPrefix(service / version / "users") {
+      get {
         pathEndOrSingleSlash {
-          entity(as[LoginRequestMessage]) { loginData ⇒
-            onComplete(UserCommands.loginByEmail(loginData)) {
-              case Success(user) ⇒ if (user) {
-                complete(StatusCodes.OK)
-              } else {
-                complete(StatusCodes.Forbidden)
-              }
-              case Failure(_) ⇒ complete(StatusCodes.NotFound)
-            }
-
-          }
+          complete(repo.all)
         }
       }
     }
   }
 
   val routes: Route =
-    createMember ~
-      loginByEmail
+    createUser ~
+      getUsers
 
 }
