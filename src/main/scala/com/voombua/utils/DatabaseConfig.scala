456684888c1634fe907ea9fcbc81b9d23cf54457
ianshiundu <ian.shiundu@gmail.com>
2018-09-25 19:26:52 +0300

    close DB connection

--- src/main/scala/com/voombua/utils/DatabaseConfig.scala
+++ src/main/scala/com/voombua/utils/DatabaseConfig.scala
@@ -1,13 +1,13 @@
 package com.voombua.utils
 
-trait DatabaseConfig {
+object DatabaseConfig {
   val driver = slick.jdbc.PostgresProfile
-  //  val driver2 = slick.jdbc.PostgresProfile
 
   import driver.api._
 
   def db = Database.forConfig("database")
 
   implicit val session: Session = db.createSession()
+  implicit val closeSession: Unit = db.close()
 
 }
