c350fd3308eb9a88045b3d053cd32ae7c449752f
ianshiundu <ian.shiundu@gmail.com>
2018-09-26 14:59:35 +0300

    update create member endpoint to use new signup method

--- src/main/scala/com/voombua/routes/UserRoutes.scala
+++ src/main/scala/com/voombua/routes/UserRoutes.scala
@@ -1,22 +1,17 @@
 package com.voombua.routes
 
-import java.sql.Timestamp
-import java.time.Instant
-import java.util.UUID
-
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
-import com.roundeights.hasher.Implicits._
-import com.voombua.core.JWT
+import com.voombua.core.AuthService
 import com.voombua.mapping.JsonProtocol
-import com.voombua.messages.LoginRequestMessage
-import com.voombua.models.{User, UserComponent}
+import com.voombua.messages.{ LoginRequestMessage, UsernameEmailPassword }
+import com.voombua.models.UserComponent
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-class UserRoutes(repo: UserComponent#UserRepository) extends JsonProtocol with JWT {
+class UserRoutes(repo: UserComponent#UserRepository, auth: AuthService) extends JsonProtocol {
   import scala.concurrent.ExecutionContext.Implicits.global
   val service = "members"
   val version = "v1"
@@ -26,10 +21,8 @@ class UserRoutes(repo: UserComponent#UserRepository) extends JsonProtocol with J
         pathEndOrSingleSlash {
           withRequestTimeout(5 minutes) {
 
-            entity(as[User]) { request ⇒
-              val r = request.copy(id = Some(UUID.randomUUID()), password = request.password.sha256.hex,
-                created = Some(Timestamp.from(Instant.now())))
-              complete((StatusCodes.Created, repo.insert(r)))
+            entity(as[UsernameEmailPassword]) { request ⇒
+              complete(StatusCodes.Created → auth.signUp(request.username, request.email, request.password))
             }
           }
         }
@@ -49,14 +42,16 @@ class UserRoutes(repo: UserComponent#UserRepository) extends JsonProtocol with J
 
   protected val logIn: Route = {
     pathPrefix(service / version / "login") {
-      pathEndOrSingleSlash {
-        entity(as[LoginRequestMessage]) { request ⇒
-          complete(
-            repo.login(request.login, request.password).map {
-              case Some(_) ⇒ StatusCodes.OK
-              case None ⇒ StatusCodes.BadRequest
-            }
-          )
+      post {
+        pathEndOrSingleSlash {
+          entity(as[LoginRequestMessage]) { request ⇒
+            complete(
+              auth.signIn(request.login, request.password).map {
+                case Some(_) ⇒ StatusCodes.OK
+                case None ⇒ StatusCodes.BadRequest
+              }
+            )
+          }
         }
       }
     }
