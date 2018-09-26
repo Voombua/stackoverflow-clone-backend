15885e0f0033ae98548577200c8fe90fd1f5a169
ianshiundu <ian.shiundu@gmail.com>
2018-09-26 23:35:02 +0300

    use HTTP route

--- src/main/scala/com/voombua/ServiceMain.scala
+++ src/main/scala/com/voombua/ServiceMain.scala
@@ -5,38 +5,37 @@ import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
-import com.voombua.core.AuthService
-import com.voombua.models.UserComponent
+import com.voombua.core.{ AuthService, QuestionService }
+import com.voombua.models.{ QuestionComponent, UserComponent }
 import com.voombua.repos.{ DB, PG }
-import com.voombua.routes.UserRoutes
+import com.voombua.routes.{ HttpRoute, UserRoutes }
 import com.voombua.utils.{ Config, MigrationConfig }
 
 import scala.concurrent.{ ExecutionContextExecutor, Future }
 
-object ServiceMain extends App with Config with MigrationConfig with UserComponent with DB with PG {
+object ServiceMain extends App with Config with MigrationConfig with UserComponent with DB with PG with QuestionComponent {
 
-  implicit val system: ActorSystem = ActorSystem() // ActorMaterializer requires an implicit ActorSystem
-  implicit val ec: ExecutionContextExecutor = system.dispatcher // bindingFuture.map requires an implicit ExecutionContext
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
   val userRepo: UserRepository = new UserRepository()
+  val questionRepo = new QuestionRepository()
   val auth = new AuthService(userRepo, "my_secret_key")
+  val questionService = new QuestionService(questionRepo)
+  val httpRoute = new HttpRoute(auth, questionService, "my_secret_key", userRepo, questionRepo).route
 
-  val api = new UserRoutes(userRepo, auth).routes // the RestApi provides a Route
-
-  implicit val materializer: ActorMaterializer = ActorMaterializer() // bindAndHandle requires an implicit materializer
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   migrate()
 
-  val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(api, httpInterface, httpPort) // starts the HTTP server
+  val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(httpRoute, httpInterface, httpPort)
 
   val log = Logging(system.eventStream, "stack-overflow-clone")
 
   try {
-    //    Here we start the HTTP server and log the info
     bindingFuture.map { serverBinding ⇒
       log.info(s"RestApi bound to ${serverBinding.localAddress}")
     }
   } catch {
-    //    If the HTTP server fails to start, we throw an Exception and log the error and close the system
     case ex: Exception ⇒
       log.error(ex, "Failed to bind to {}:{}!", httpInterface, httpPort)
       //      System shutdown
