f2f8430a764b0809dfe9bcfb4a103d1fc1909969
ianshiundu <ian.shiundu@gmail.com>
2018-09-26 14:57:21 +0300

    add auth trait and implement sign up

--- src/main/scala/com/voombua/models/UserComponent.scala
+++ src/main/scala/com/voombua/models/UserComponent.scala
@@ -1,16 +1,16 @@
 package com.voombua.models
 
+import com.roundeights.hasher.Implicits._
 import com.voombua.repos.{ DB, RepoDefinition }
 
 import scala.concurrent.{ ExecutionContext, Future }
-import com.roundeights.hasher.Implicits._
 trait UserComponent extends RepoDefinition { this: DB ⇒
   import driver.api._
   class UsersTable(tag: Tag) extends BaseTable[User](tag, "users") {
     val username = column[String]("username")
     val email = column[String]("email")
     val password = column[String]("password")
-    def * = (id.?, username, email, password, created.?, updated.?, deleted.?) <> ((User.apply _).tupled, User.unapply)
+    def * = (id, username, email, password, created, updated.?, deleted.?) <> ((User.apply _).tupled, User.unapply)
   }
 
   class UserRepository(implicit ex: ExecutionContext) extends BaseRepo[User, UsersTable] with Authentication {
@@ -19,16 +19,14 @@ trait UserComponent extends RepoDefinition { this: DB ⇒
     override def login(login: String, password: String): Future[Option[User]] =
       db.run(table.filter(d => d.username === login || d.email === login && d.password === password.sha256.hex).result.headOption)
 
+    override def UserSignUp(authData: User): Future[User] =
+      db.run(table.insertOrUpdate(authData)).map(_ => authData)
   }
 
 }
 
 sealed trait Authentication {
   def login(login: String, password: String): Future[Option[User]]
+  def UserSignUp(authData: User): Future[User]
 }
-
-//class Auth(ex: ExecutionContext) extends Authentication {
-//  override def login(login: String): Future[Option[User]] =
-//
-//}
 
