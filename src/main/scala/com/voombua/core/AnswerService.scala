e99b2cc339cddca3677caaaeba4c8ef8f5c8c5e0
ianshiundu <ian.shiundu@gmail.com>
2018-09-28 11:48:16 +0300

    implement update answer logic

--- src/main/scala/com/voombua/core/AnswerService.scala
+++ src/main/scala/com/voombua/core/AnswerService.scala
@@ -4,8 +4,9 @@ import java.sql.Timestamp
 import java.time.Instant
 import java.util.UUID
 
-import com.voombua.messages.AnswerMessage
+import com.voombua.messages.{ AnswerMessage, AnswerUpdateMessage }
 import com.voombua.models.{ Answer, AnswerComponent }
+import com.voombua.utils.MonadTransformers._
 
 import scala.concurrent.{ ExecutionContext, Future }
 
@@ -13,5 +14,12 @@ class AnswerService(answerStorage: AnswerComponent#AnswersRepository)(implicit e
   def postAnswer(request: AnswerMessage, questionId: QuestionId, userId: UserId): Future[Answer] = {
     answerStorage.saveAnswer(Answer(UUID.randomUUID().toString, questionId, userId, request.content,
       Timestamp.from(Instant.now()), None, None))
+  }
+
+  def updateAnswer(answerId: AnswerId, request: AnswerUpdateMessage): Future[Option[Answer]] = {
+    answerStorage
+      .findAnswer(answerId)
+      .mapT(request.merge)
+      .flatMapTOuter(answerStorage.saveAnswer)
   }
 }
